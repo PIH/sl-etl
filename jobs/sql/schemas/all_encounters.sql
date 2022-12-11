f4736d041b66c2194abb086169615ff20db59143
aibrahem550 <ahmed.ibrahem550@gmail.com>
2022-12-11 20:43:51 +0200

    add all_encounters table

---
+++ jobs/sql/schemas/all_encounters.sql
@@ -0,0 +1,10 @@
+create table all_encounters(
+encounter_id int,
+wellbody_emr_id varchar(50),
+kgh_emr_id varchar(50),
+encounter_type varchar(50),
+encounter_datetime datetime,
+year int,
+month int,
+created_by varchar(30)
+);
