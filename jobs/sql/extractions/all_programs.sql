491a2a7c4f6ad2ba7c62af54dc6f2e4e3c09879f
aibrahem550 <ahmed.ibrahem550@gmail.com>
2022-12-14 11:34:38 +0200

    commit

--- jobs/sql/extractions/all_programs.sql
+++ jobs/sql/extractions/all_programs.sql
@@ -5,11 +5,11 @@ SELECT patient_identifier_type_id INTO @kgh_identifier_type FROM patient_identif
 
 DROP temporary TABLE IF EXISTS all_programs;
 create temporary table all_programs(
-patient_id int, 
+patient_id int,
 wellbody_emr_id varchar(50),
 kgh_emr_id varchar(50),
 program_name varchar(50),
-date_enrolled date, 
+date_enrolled date,
 date_completed date,
 final_program_status varchar(100),
 created_by varchar(50)
