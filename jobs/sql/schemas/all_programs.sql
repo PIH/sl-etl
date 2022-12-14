f950565d9f3fdfc16ef7f6ad783176404d43eb53
dmdesimone <ddesimone@pih.org>
2022-12-14 09:39:11 -0500

    recreate all_programs

---
+++ jobs/sql/schemas/all_programs.sql
@@ -0,0 +1,9 @@
+create table #all_programs(
+  wellbody_emr_id       varchar(50),
+  kgh_emr_id            varchar(50),
+  program_name          varchar(50),
+  date_enrolled         date,
+  date_completed        date,
+  final_program_status  varchar(100),
+  created_by            varchar(50)
+);
