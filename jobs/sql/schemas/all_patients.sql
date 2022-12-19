9b296ccc0fa1b3510e5d9c5d5405ab7bdc5d1ca5
aibrahem550 <ahmed.ibrahem550@gmail.com>
2022-12-19 19:36:42 +0200

    commit

---
+++ jobs/sql/schemas/all_patients.sql
@@ -0,0 +1,18 @@
+CREATE TABLE  all_patients 
+(
+wellbody_emr_id varchar(50),
+kgh_emr_id varchar(50),
+patient_id int, 
+reg_location varchar(50),
+reg_date date,
+fist_encounter_date date,
+last_encounter_date date, 
+name varchar(50),
+family_name varchar(50),
+dob date,
+dob_estimated bit,
+gender varchar(2),
+dead bit,
+death_date date,
+cause_of_death varchar(100)
+);
