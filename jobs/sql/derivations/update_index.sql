35ea00907f96bde28c06400d2727bdf930299043
aibrahem550 <ahmed.ibrahem550@gmail.com>
2023-03-12 18:43:56 +0200

    commit

--- jobs/sql/derivations/update_index.sql
+++ jobs/sql/derivations/update_index.sql
@@ -1,5 +1,5 @@
 -- update index asc/desc on all_encounters table
-select  patient_id, encounter_datetime,encounter_type_id,
+{# select  patient_id, encounter_datetime,encounter_type_id,
 ROW_NUMBER() over (PARTITION by patient_id order by  encounter_type_id asc, encounter_datetime asc) "index_asc",
 ROW_NUMBER() over (PARTITION by patient_id order by encounter_type_id desc, encounter_datetime DESC) "index_desc"
 into #all_encounters_indexes
@@ -11,4 +11,4 @@ set av.index_asc = avi.index_asc,
 from all_encounters av
 inner join #all_encounters_indexes avi on avi.patient_id = av.patient_id
 and avi.encounter_type_id = av.encounter_type_id
-and avi.encounter_datetime = av.encounter_datetime; 
+and avi.encounter_datetime = av.encounter_datetime;  #}
