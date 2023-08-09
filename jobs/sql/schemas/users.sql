49a3cde969bea7507a2f8def70cf27a05c18f4d3
Michael Seaton <mseaton@pih.org>
2023-08-09 08:36:36 -0400

    Add mfa_status column

--- jobs/sql/schemas/users.sql
+++ jobs/sql/schemas/users.sql
@@ -8,5 +8,6 @@ CREATE TABLE users
     created_by          varchar(50),
     provider_type       varchar(50),
     last_login_date     datetime,
-    num_logins_recorded int
+    num_logins_recorded int,
+    mfa_status          varchar(50)
 );
