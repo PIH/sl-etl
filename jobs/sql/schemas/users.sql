50023d6d7e89ffcf3f0361d9bef3dc8e418670a1
aibrahem550 <ahmed.ibrahem550@gmail.com>
2023-12-08 09:47:16 +0200

    commit

--- jobs/sql/schemas/users.sql
+++ jobs/sql/schemas/users.sql
@@ -3,6 +3,7 @@ CREATE TABLE users
     username            varchar(50),
     first_name          varchar(50),
     last_name           varchar(50),
+    email               varchar(500),
     account_enabled     bit,
     created_date        datetime,
     created_by          varchar(50),
@@ -10,4 +11,5 @@ CREATE TABLE users
     last_login_date     datetime,
     num_logins_recorded int,
     mfa_status          varchar(50)
-);
+)
+;
