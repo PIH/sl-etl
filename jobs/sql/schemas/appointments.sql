7927cb53fcad8a56d2bfce7f835695c678cfe355
Michael Seaton <mseaton@pih.org>
2024-08-23 09:05:43 -0400

    SL-715 - Extract table for appointments

---
+++ jobs/sql/schemas/appointments.sql
@@ -0,0 +1,20 @@
+create table appointments
+(
+    appointment_id       varchar(25),
+    patient_id           varchar(25),
+    emr_id               varchar(50),
+    date_issued          date,
+    location             varchar(255),
+    service              varchar(50),
+    type                 varchar(45),
+    recurring            bit,
+    appointment_datetime datetime,
+    duration             int,
+    provider             varchar(255),
+    status               varchar(45),
+    note                 varchar(255),
+    datetime_created     datetime,
+    user_entered         varchar(255),
+    index_asc            int,
+    index_desc           int
+);
