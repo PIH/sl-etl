da6a185cb87a2df7f5c1a7dce48408fac60b95b6
dmdesimone <ddesimone@pih.org>
2024-09-18 10:21:20 -0400

    SL-726 new mother_newborn_relationship table

---
+++ jobs/sql/schemas/mother_newborn_relationship.sql
@@ -0,0 +1,13 @@
+ create table mother_newborn_relationship
+(
+ relationship_id int,
+ emr_id varchar(255),
+ patient_id int,
+ emr_id_mother varchar(255), 
+ patient_id_mother int,
+ relationship_source varchar(255), 
+ user_entered text,         
+ date_created datetime,     
+ index_asc int,          
+ index_desc int
+); 
