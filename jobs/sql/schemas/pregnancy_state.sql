f18150a915837726ee916bae7d7598d239b1fa17
dmdesimone <ddesimone@pih.org>
2024-10-11 10:40:35 -0400

    SL-716-Pregnancy-state

--- jobs/sql/schemas/pregnancy_state.sql
+++ jobs/sql/schemas/pregnancy_state.sql
@@ -1,16 +1,18 @@
 create table pregnancy_state
 (
-pregnancy_program_state_id varchar(30),
-emr_id varchar(50),
-patient_id varchar(30),
-pregnancy_program_id varchar(30),
-state varchar(255),
-user_entered varchar(255),
-date_entered datetime,
-state_start_date datetime,
-state_end_date datetime,
-program_enrollment_date datetime,
-program_completion_date datetime,
-index_asc int,
-index_desc int
+pregnancy_program_state_id varchar(30),  
+emr_id                     varchar(50),  
+patient_id                 varchar(30),  
+pregnancy_program_id       varchar(30),  
+state                      varchar(255), 
+user_entered               varchar(255), 
+date_entered               datetime,     
+state_start_date           date,         
+state_end_date             date,         
+program_enrollment_date    datetime,     
+program_completion_date    datetime,     
+index_asc_patient          int,           
+index_desc_patient         int,           
+index_asc_patient_program  int,           
+index_desc_patient_program int           
 );
