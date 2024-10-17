5d14af00328ac4264ffe5b8ec4901206e9846941
dmdesimone <ddesimone@pih.org>
2024-10-17 16:28:30 -0400

    UHM-8227 appointments table failing on sl_warehouse on PETL-TEST

---
+++ jobs/sql/extractions/functions/appointments.sql
@@ -0,0 +1,18 @@
+#
+-- This will return the date appointment scheduled for a given patient appoointment id
+-- if that column does not exist, return null
+#
+CREATE FUNCTION date_appointment_scheduled(_patient_appointment_id int) RETURNS datetime
+    DETERMINISTIC
+BEGIN
+    DECLARE ret datetime;
+
+IF column_exists('patient_appointment','date_appointment_scheduled') THEN
+ select date_appointment_scheduled into ret
+ 	from patient_appointment where patient_appointment_id = _patient_appointment_id	;
+ELSE
+ set ret = null ;
+END IF;
+
+    RETURN ret;
+END
