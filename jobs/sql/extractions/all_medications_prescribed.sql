7fbcc31377c12bef9040e30affeeb06f05027778
dmdesimone <ddesimone@pih.org>
2025-04-16 16:28:11 -0400

    SL-916-refactor-for-performance

--- jobs/sql/extractions/all_medications_prescribed.sql
+++ jobs/sql/extractions/all_medications_prescribed.sql
@@ -222,7 +222,7 @@ UPDATE all_medication_prescribed SET emr_id=patient_identifier(patient_id,@prima
 
 SELECT 
     medication_prescription_id,
-	concat(@partition, '-', order_id) as order_id,
+    concat(@partition, '-', order_id) as order_id,
     concat(@partition,'-',obs_group_id) as 'obs_id',
     concat(@partition, '-', encounter_id) as encounter_id,
     concat(@partition, '-', visit_id) as visit_id,
