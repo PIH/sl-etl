81e02c1d6a7680d52242d635b9ffd96fe93ae419
dmdesimone <ddesimone@pih.org>
2026-01-23 09:23:22 -0500

    SL-1159 add mcoe_flag to all_encounters
    Added location variables for various departments and updated the all_encounters table to include mcoe_location.

--- jobs/sql/extractions/all_encounters.sql
+++ jobs/sql/extractions/all_encounters.sql
@@ -3,6 +3,21 @@ set @partition = '${partitionNum}';
 SELECT patient_identifier_type_id INTO @identifier_type FROM patient_identifier_type pit WHERE uuid ='1a2acce0-7426-11e5-a837-0800200c9a66';
 SELECT patient_identifier_type_id INTO @kgh_identifier_type FROM patient_identifier_type pit WHERE uuid ='c09a1d24-7162-11eb-8aa6-0242ac110002';
 
+select location_id into @anc from location where uuid = '11f5c9f9-40b8-46ad-9e7e-59473ce43246';
+select location_id into @labour from location where uuid = '11377a5b-6850-11ee-ab8d-0242ac120002';
+select location_id into @nicu from location where uuid = '0ce2f6fb-6850-11ee-ab8d-0242ac120002';
+select location_id into @pacu from location where uuid = '17596678-6850-11ee-ab8d-0242ac120002';
+select location_id into @pnc from location where uuid = 'ff0d5e73-3fe0-437f-90ba-7d605ac03dc0';
+select location_id into @quiet from location where uuid = '28660b7f-3450-4b86-b840-9670ec68235f';
+select location_id into @mccu from location where uuid = '4d7e927d-6850-11ee-ab8d-0242ac120002';
+select location_id into @postop from location where uuid = 'a39ec469-d1f9-11f0-9d46-169316be6a48';
+select location_id into @preop from location where uuid = '142de844-6850-11ee-ab8d-0242ac120002';
+select location_id into @kgh_mch from location where uuid = '5981f962-6eec-453d-89ce-2f9ac48d096f';
+select location_id into @mcoe_pharmacy from location where uuid = '550e8400-e29b-41d4-a716-446655440000';
+select location_id into @mcoe_registration from location where uuid = '07aa9943-d1fa-11f0-9d46-169316be6a48';
+select location_id into @mcoe_triage from location where uuid = 'f85feffc-fe54-4648-aa14-01ed6d30b943';
+
+
 DROP temporary TABLE IF EXISTS all_encounters;
 create temporary table all_encounters(
 encounter_id int,
@@ -17,6 +32,7 @@ provider varchar(50),
 encounter_datetime datetime,
 location_id int(11),
 encounter_location varchar(255),
+mcoe_location boolean,
 encounter_year int,
 encounter_month int,
 datetime_entered datetime,
@@ -76,6 +92,12 @@ SET ae.provider=provider(ae.encounter_id);
 UPDATE all_encounters ae 
 SET ae.encounter_location = location_name(ae.location_id);
 
+UPDATE all_encounters ae 
+SET ae.mcoe_location = 1
+where ae.location_id in (@anc, @labour, @nicu, @pacu, @pnc, @quiet,
+  @mccu, @postop, @preop, @kgh_mch,
+  @mcoe_pharmacy, @mcoe_registration, @mcoe_triage);
+
 select 
 concat(@partition,"-",encounter_id) as encounter_id,
 concat(@partition,"-",patient_id)  as patient_id,
@@ -86,6 +108,7 @@ emr_id,
 encounter_type,
 encounter_type_id,
 encounter_location,
+mcoe_location,
 provider,
 encounter_datetime,
 datetime_entered,
