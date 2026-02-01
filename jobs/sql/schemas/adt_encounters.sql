1a122eefda108db15db6c9ae61f0d267f3501a69
dmdesimone <ddesimone@pih.org>
2026-02-01 18:30:01 -0500

    Uhm 9080 add adt encounters (#374)
    * UHM-9080 add adt_encounters
    
    * UHM-9080-add-adt_encounters
    
    * UHM-9080-add-adt_encounters
    
    * UHM-9080-add-adt_encounters
    
    Add logic to update index ascending and descending values for adt_encounters.
    
    * UHM-9080-add-adt_encounters
    
    * UHM-9080-add-adt_encounters

---
+++ jobs/sql/schemas/adt_encounters.sql
@@ -0,0 +1,15 @@
+create table adt_encounters
+(
+	  emr_id               varchar(15),
+    encounter_id         varchar(255),
+    visit_id             varchar(255),
+    encounter_datetime   datetime,
+    user_entered         varchar(255),
+    datetime_created     datetime,
+    encounter_type       varchar(255),
+    encounter_location   varchar(255),
+    mcoe_location        bit,
+    provider             varchar(255),
+    index_asc            int,
+    index_desc           int
+);
