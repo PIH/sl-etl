cd900a9e6416942e2adfe88f24e35827c3f19976
dmdesimone <ddesimone@pih.org>
2026-02-07 22:44:06 -0500

    UHM-9080 add columns to all admissions table (#388)
    Added hospital start and end datetime columns and updated insertion logic to include patient_id and mcoe_location.

--- jobs/sql/derivations/all_admissions.sql
+++ jobs/sql/derivations/all_admissions.sql
@@ -1,15 +1,19 @@
 DROP TABLE IF EXISTS all_admissions_staging;
 CREATE TABLE all_admissions_staging
 (
+  patient_id                        varchar(50),
   emr_id                            varchar(15), 
   encounter_id                      varchar(50), 
   visit_id                          varchar(255),
   encounter_type                    varchar(50), 
   start_datetime                    datetime,    
-  end_datetime                      datetime,    
+  end_datetime                      datetime,   
+  hospital_start_datetime           datetime,
+  hospital_end_datetime             datetime,
   user_entered                      varchar(255),
   date_entered                      date,        
   encounter_location                varchar(255),
+  mcoe_location                     bit,
   provider                          varchar(255),
   previous_disposition_encounter_id varchar(50), 
   previous_disposition_datetime     datetime,    
@@ -22,10 +26,11 @@ CREATE TABLE all_admissions_staging
 );
 
 INSERT INTO all_admissions_staging (
-  emr_id, encounter_id, visit_id, encounter_type, start_datetime,
-  end_datetime, user_entered, date_entered, encounter_location, provider, site, partition_num
+  patient_id, emr_id, encounter_id, visit_id, encounter_type, start_datetime,
+  end_datetime, user_entered, date_entered, encounter_location, mcoe_location, provider, site, partition_num
 )
 SELECT
+  patient_id, 
   emr_id,
   encounter_id,
   visit_id,
@@ -35,6 +40,7 @@ SELECT
   user_entered AS creator,
   datetime_created AS date_entered,
   encounter_location,
+  mcoe_location,
   provider,
   site,
   partition_num
@@ -72,6 +78,7 @@ inner join all_encounters e on e.encounter_id =
 	(select top 1 e2.encounter_id from all_encounters e2
 	where e2.emr_id = a.emr_id 
 	and e2.encounter_datetime <= a.end_datetime
+	and e2.encounter_datetime >= a.start_datetime
 	and e2.disposition is not null
 	order by e2.encounter_datetime desc, e2.encounter_id desc);	
 
@@ -82,5 +89,17 @@ FROM all_admissions_staging a
 INNER JOIN all_visits v ON a.visit_id = v.visit_id
 WHERE a.end_datetime IS NULL;
 
+drop table if exists #temp_hospital_datetimes;
+select patient_id, visit_id, min(start_datetime) "min_start_datetime", max(end_datetime) "max_start_datetime"
+ into #temp_hospital_datetimes
+from all_admissions_staging
+group by patient_id, visit_id; 
+
+update a
+set hospital_start_datetime = min_start_datetime,
+	hospital_end_datetime = max_start_datetime
+FROM all_admissions_staging a
+INNER JOIN #temp_hospital_datetimes t ON t.visit_id = a.visit_id and t.patient_id = a.patient_id;
+  
 DROP TABLE IF EXISTS all_admissions;
 EXEC sp_rename 'dbo.all_admissions_staging', 'all_admissions';
