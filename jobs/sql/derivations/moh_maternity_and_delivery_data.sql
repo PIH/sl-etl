f62a7b1bf107f0b29ec0b6ba4aff01cdede7cbcc
dmdesimone <ddesimone@pih.org>
2026-02-12 20:51:54 -0500

    SL-1192-create-moh_maternity_and_delivery_data-table
    This script creates a staging table for the MOH maternity and delivery report, aggregates data, and updates gestational age and breastfeeding initiation details.

---
+++ jobs/sql/derivations/moh_maternity_and_delivery_data.sql
@@ -0,0 +1,74 @@
+-- this script creates a table that contains the data for the moh maternity and delivery report
+-- this is aggregated in a downstream script to create the final table
+
+-- indicators at the delivered baby level (mch_delivery_summary_encounter)
+drop TABLE if exists moh_maternity_and_delivery_data_staging;
+create table moh_maternity_and_delivery_data_staging
+(site                             varchar(50),  
+baby_obs_id                       varchar(50),  
+pregnancy_program_id              varchar(50),  
+reporting_date                    date,         
+age_at_encounter                  int,          
+age_category                      varchar(50),  
+outcome                           varchar(255), 
+birth_weight                      float,        
+delivery_method                   varchar(255), 
+gestational_age                   int,          
+method_of_induction               varchar(255), 
+partogram_uploaded                bit,          
+birthdate                         datetime,     
+breastfeeding_initiation_datetime datetime      
+);
+
+-- disaggregate deliveries into reporting_date, age categories
+insert into moh_maternity_and_delivery_data_staging (site, baby_obs_id, pregnancy_program_id, reporting_date, age_at_encounter, age_category, outcome, birth_weight, delivery_method, birthdate)
+select 
+site, 
+baby_obs_id,
+pregnancy_program_id,
+dd.LastDayOfMonth, 
+mother_age_at_encounter,
+CASE
+	when mother_age_at_encounter < 0 then '<10'
+	when mother_age_at_encounter < 15 then '10-14'
+	when mother_age_at_encounter < 20 then '15-19'
+	when mother_age_at_encounter < 26 then '20-25'
+	else'26+'
+END "age_category",
+outcome, 
+birth_weight, 
+delivery_method,
+birthdate
+from mch_delivery_summary_encounter d
+inner join dim_date dd on dd.Date = cast(d.encounter_datetime as date);
+
+update d 
+set d.breastfeeding_initiation_datetime = l.breastfeeding_initiation_datetime
+from moh_maternity_and_delivery_data_staging d 
+inner join mch_labor_summary_encounter l on l.encounter_id = 
+(select top 1 l2.encounter_id  
+from mch_labor_summary_encounter l2
+where l2.pregnancy_program_id = d.pregnancy_program_id
+order by l2.encounter_datetime desc)
+where d.gestational_age is null;
+
+update d 
+set d.gestational_age = l.gestational_age,
+	d.method_of_induction = l.method_of_induction,
+	d.partogram_uploaded = l.partogram_uploaded
+from moh_maternity_and_delivery_data_staging d 
+inner join mch_labor_progress_encounter l on l.encounter_id = 
+(select top 1 l2.encounter_id  
+from mch_labor_progress_encounter l2
+where l2.pregnancy_program_id = d.pregnancy_program_id
+order by l2.encounter_datetime desc)
+where d.gestational_age is null;
+
+update d 
+set d.gestational_age = p.estimated_gestational_age
+from moh_maternity_and_delivery_data_staging d 
+inner join mch_pregnancy_summary p on p.pregnancy_program_id = d.pregnancy_program_id 
+where d.gestational_age is null;
+
+DROP TABLE IF EXISTS moh_maternity_and_delivery_data;
+EXEC sp_rename 'moh_maternity_and_delivery_data_staging', 'moh_maternity_and_delivery_data';
