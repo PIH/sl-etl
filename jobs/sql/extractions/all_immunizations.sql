f085304ef336316bb9ba4245b6011084c7229330
dmdesimone <ddesimone@pih.org>
2026-02-16 18:56:25 -0500

    Sl 1196 add mcoe location to all immunizations (#403)
    * SL-1196 add mcoe_location to all_immunizations
    
    * SL-1196-add-mcoe_location-to-all_immunizations

--- jobs/sql/extractions/all_immunizations.sql
+++ jobs/sql/extractions/all_immunizations.sql
@@ -1,6 +1,23 @@
 set @immunization = concept_from_mapping('PIH','10156');
 set @pregnancyProgramId = program('Pregnancy');
 set @partition = '${partitionNum}';
+select location_id into @anc from location where uuid = '11f5c9f9-40b8-46ad-9e7e-59473ce43246';
+select location_id into @labour from location where uuid = '11377a5b-6850-11ee-ab8d-0242ac120002';
+select location_id into @nicu from location where uuid = '0ce2f6fb-6850-11ee-ab8d-0242ac120002';
+select location_id into @pacu from location where uuid = '17596678-6850-11ee-ab8d-0242ac120002';
+select location_id into @pnc from location where uuid = 'ff0d5e73-3fe0-437f-90ba-7d605ac03dc0';
+select location_id into @quiet from location where uuid = '28660b7f-3450-4b86-b840-9670ec68235f';
+select location_id into @mccu from location where uuid = '4d7e927d-6850-11ee-ab8d-0242ac120002';
+select location_id into @postop from location where uuid = 'a39ec469-d1f9-11f0-9d46-169316be6a48';
+select location_id into @preop from location where uuid = '142de844-6850-11ee-ab8d-0242ac120002';
+select location_id into @kgh_mch from location where uuid = '5981f962-6eec-453d-89ce-2f9ac48d096f';
+select location_id into @mcoe_pharmacy from location where uuid = '550e8400-e29b-41d4-a716-446655440000';
+select location_id into @mcoe_registration from location where uuid = '07aa9943-d1fa-11f0-9d46-169316be6a48';
+select location_id into @mcoe_triage from location where uuid = 'f85feffc-fe54-4648-aa14-01ed6d30b943';
+select location_id into @mothers_dorm from location where uuid = '989a9b23-d1f9-11f0-9d46-169316be6a48';
+select location_id into @staff from location where uuid = 'adde966c-d1f9-11f0-9d46-169316be6a48';
+select location_id into @kangaroo from location where uuid = '81080213-d1f9-11f0-9d46-169316be6a48';
+
 
 drop temporary table if exists temp_immunizations;
 create temporary table temp_immunizations
@@ -12,6 +29,7 @@ visit_id                   int,
 pregnancy_program_id       int, 
 encounter_datetime         datetime, 
 encounter_location         varchar(255), 
+mcoe_location              bit,
 datetime_entered           datetime,     
 user_entered               varchar(255), 
 provider                   varchar(255),
@@ -39,6 +57,7 @@ encounter_id               int,
 visit_id                   int,
 encounter_datetime         datetime,     
 location_id                int,
+mcoe_location              bit,
 encounter_location         varchar(255), 
 datetime_entered           datetime,
 creator                    int,
@@ -86,6 +105,11 @@ set age_at_encounter = age_at_enc(patient_id, encounter_id);
 UPDATE temp_immunizations_encounter
 set pregnancy_program_id = patient_program_id_from_encounter(patient_id, @pregnancyProgramId ,encounter_id);
 
+UPDATE temp_immunizations_encounter t 
+SET mcoe_location = 1
+where t.location_id in (@anc, @labour, @nicu, @pacu, @pnc, @quiet, @mccu, @postop, @preop, @kgh_mch,
+  @mcoe_pharmacy, @mcoe_registration, @mcoe_triage, @mothers_dorm, @staff, @kangaroo);
+
 update temp_immunizations i 
 inner join temp_immunizations_encounter e on e.encounter_id = i.encounter_id 
 set i.emr_id = e.emr_id,
@@ -93,6 +117,7 @@ set i.emr_id = e.emr_id,
 	i.visit_id = e.visit_id,
 	i.encounter_datetime = e.encounter_datetime,
 	i.encounter_location = e.encounter_location,
+	i.mcoe_location = e.mcoe_location,
 	i.datetime_entered = e.datetime_entered,
 	i.user_entered = e.user_entered,
 	i.provider = e.provider,
@@ -112,6 +137,7 @@ emr_id,
 concat(@partition, '-', pregnancy_program_id) as pregnancy_program_id,
 encounter_datetime,
 encounter_location,
+mcoe_location,
 datetime_entered,
 user_entered,
 provider,
