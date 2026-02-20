8a5d44e1bdc7213b8f747baaa85224447760c101
dmdesimone <ddesimone@pih.org>
2026-02-20 13:36:54 -0500

    SL-1209 create mcoe tables

---
+++ jobs/sql/derivations/create_mcoe_tables.sql
@@ -0,0 +1,19 @@
+drop table if exists mcoe_encounters;
+select * 
+into mcoe_encounters
+from all_encounters 
+where mcoe_location = 1;
+
+drop table if exists mcoe_diagnosis;
+select d.* 
+into mcoe_diagnosis 
+from all_diagnosis d 
+inner join mcoe_encounters m on m.encounter_id = d.encounter_id 
+
+drop table if exists mcoe_visits;
+select * 
+into mcoe_visits
+from all_visits v 
+where exists 
+(select 1 from mcoe_encounters e 
+where e.visit_id = v.visit_id);
