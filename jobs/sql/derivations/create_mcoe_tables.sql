604084ce98b5952bf6b6cc725a55df7cc89c5c38
dmdesimone <ddesimone@pih.org>
2026-02-20 14:13:13 -0500

    Sl 1209 create mcoe tables (#409)
    * SL-1209 create mcoe tables
    
    * SL-1209-create-mcoe-tables
    
    * SL-1209-create-mcoe-tables
    
    Drop mcoe_vitals table if it exists and create it from all_vitals joined with mcoe_encounters.

--- jobs/sql/derivations/create_mcoe_tables.sql
+++ jobs/sql/derivations/create_mcoe_tables.sql
@@ -17,3 +17,9 @@ from all_visits v
 where exists 
 (select 1 from mcoe_encounters e 
 where e.visit_id = v.visit_id);
+
+drop table if exists mcoe_vitals;
+select v.* 
+into mcoe_vitals 
+from all_vitals v 
+inner join mcoe_encounters m on m.encounter_id = v.encounter_id;
