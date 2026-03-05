f1bba2fe79c656c69742ebd27ac939dea9336ebe
dmdesimone <ddesimone@pih.org>
2026-03-05 12:15:09 -0500

    Sl 1224 new moh postnatal care data table (#423)
    * SL-1224 new  moh_postnatal_care_data table
    
    This script creates a staging table for postnatal care data, inserts outpatient and inpatient follow-up records, updates reporting dates and timeframes after delivery, and renames the staging table to the final table.
    
    * SL-1224 rework moh postnatal care table
    
    Refactor postnatal care SQL logic to include gender-specific neonate counts and update column names.
    
    * SL-1224-new-moh_postnatal_care_data-table
    
    * SL-1224-new-moh_postnatal_care_data-table

--- jobs/sql/derivations/moh_postnatal_care.sql
+++ jobs/sql/derivations/moh_postnatal_care.sql
@@ -1,50 +1,11 @@
-drop  table if exists #temp_postnatal_encounters;
-select e.site, dd.LastDayOfMonth, e.patient_id , datediff(day, e2.encounter_datetime, e.encounter_datetime) "days_diff"
-into #temp_postnatal_encounters
-from all_encounters e
-inner join all_encounters e2 on e2.patient_id = e.patient_id  
-	and e2.encounter_type = 'Labor and Delivery Summary'
-	and e2.encounter_datetime < e.encounter_datetime 
-	and datediff(day, e2.encounter_datetime, e.encounter_datetime) < 42
-inner join dim_date dd on dd.Date = cast(e.encounter_datetime as date)	
-where e.encounter_type in ('ANC Followup', 'ANC Intake'); 
-
-drop table if exists #temp_min_days_after_delivery;
-create table #temp_min_days_after_delivery
-(site          varchar(50),
-reporting_date date,
-patient_id     varchar(20),
-min_days_after int,
-timeframe      varchar(20));
-
-insert into #temp_min_days_after_delivery (site, reporting_date, patient_id, min_days_after)
-select site, LastDayOfMonth, patient_id, min(days_diff) "min_days_after" 
-from #temp_postnatal_encounters
-group by site, LastDayOfMonth, patient_id;
-	
-update #temp_min_days_after_delivery
-set timeframe = 
-case 
-	when min_days_after <= 1 then 'within 24 hours'
-	when min_days_after <= 7 then '2-7 days'
-	else  '8 days to 6 weeks'
-end	;
-
-drop table if exists #days_after_counts;
-select site, reporting_date, timeframe, count(*) "count" 
-into #days_after_counts
-FROM  #temp_min_days_after_delivery
-group by site, reporting_date, timeframe;
-
 drop table if exists final_table_staging;
 create table final_table_staging
-(site                     varchar(50),
-reporting_date            date,
-timeframe                 varchar(20),
-mother_in_facility_count  int,
-mother_outreach_count     int,
-neonate_in_facility_count int,
-neonate_outreach_count    int);
+(site                      varchar(50),
+reporting_date             date,
+timeframe                  varchar(20),
+mother_in_facility         int,
+neonate_in_facility_male   int,
+neonate_in_facility_female int);
 
 insert into final_table_staging (site, reporting_date, timeframe)
 select distinct site, LastDayOfMonth, timeframe from  dim_date   
@@ -57,16 +18,30 @@ cross join
 where LastDayOfMonth >= '2023-01-01' and Date <= GETDATE();
 
 update f 
-set f.mother_in_facility_count = c.count
-from final_table_staging f
-inner join #days_after_counts c on c.site = f.site
-	and c.reporting_date = f.reporting_date 
-	and c.timeframe = f.timeframe;
+set f.mother_in_facility = i.count
+from final_table_staging f 
+inner join 
+	(select site, reporting_date, timeframe_after_delivery, count(distinct mother_patient_id) "count"
+	from moh_postnatal_care_data
+	group by site, reporting_date, timeframe_after_delivery) i 
+	on i.site = f.site  and i.reporting_date = f.reporting_date and i.timeframe_after_delivery = f.timeframe;
+
+update f set f.mother_in_facility = 0 from final_table_staging f where f.mother_in_facility is null;
 
 update f 
-set f.mother_in_facility_count = 0
-from final_table_staging f
-where f.mother_in_facility_count is null;
+set f.neonate_in_facility_male = i.male_baby_count,
+	f.neonate_in_facility_female = i.female_baby_count
+from final_table_staging f 
+inner join 
+	(select site, reporting_date, timeframe_after_delivery, 
+	count(distinct case when baby_gender = 'Male' then baby_patient_id else null end) "male_baby_count",
+	count(distinct case when baby_gender = 'Female' then baby_patient_id else null end) "female_baby_count"
+	from moh_postnatal_care_data
+	group by site, reporting_date, timeframe_after_delivery) i 
+	on i.site = f.site  and i.reporting_date = f.reporting_date and i.timeframe_after_delivery = f.timeframe;
+
+update f set f.neonate_in_facility_male = 0 from final_table_staging f where f.neonate_in_facility_male is null;
+update f set f.neonate_in_facility_female = 0 from final_table_staging f where f.neonate_in_facility_female is null;
 
 DROP TABLE IF EXISTS moh_postnatal_care;
 EXEC sp_rename 'final_table_staging', 'moh_postnatal_care';
