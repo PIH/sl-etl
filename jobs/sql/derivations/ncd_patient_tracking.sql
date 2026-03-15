f056ad7518423237ac84f53de18e580b4b996bbf
dmdesimone <ddesimone@pih.org>
2026-03-14 22:24:01 -0400

    SL-1240 Add NCD patient tracking SQL script
    Create a staging table for tracking NCD patients and calculate days since last visit and days late for appointment. Update the table with days since lost to follow-up before renaming it.

---
+++ jobs/sql/derivations/ncd_patient_tracking.sql
@@ -0,0 +1,48 @@
+drop table if exists ncd_patient_tracking_staging;
+select
+p.emr_id,
+concat(name, ' ', family_name) "name", 
+datediff(year, p.birthdate, GETDATE()) "current_age", 
+full_address,
+district,
+telephone_number,
+primary_contact_name,
+primary_contact_number,
+local_contact_name,
+local_contact_number,
+referred_from,
+all_conditions,
+most_recent_visit_date,
+first_ncd_visit_date,
+next_appointment_date,
+diabetes,
+hypertension,
+heart_failure, 
+chronic_lung_disease, 
+chronic_kidney_disease, 
+liver_cirrhosis_hepb,
+palliative_care,
+sickle_cell,
+other_ncd,
+diabetes_type,
+date_enrolled,
+DATEDIFF(day, COALESCE(most_recent_visit_date, date_enrolled), GETDATE()) "days_since_last_visit",
+DATEDIFF(day, COALESCE(next_appointment_date, date_enrolled), GETDATE()) "days_late_for_appointment"
+into ncd_patient_tracking_staging
+from all_patients p 
+inner join ncd_patient n on n.patient_id = p.patient_id 
+	and n.calculated_reporting_outcome = 'Lost to followup';
+
+alter table ncd_patient_tracking_staging
+add days_since_ltfu int;
+
+update n 
+set days_since_ltfu = 
+case
+	when (days_since_last_visit - 90) < (days_late_for_appointment - 180) then (days_since_last_visit - 90)
+	else (days_late_for_appointment - 180)
+end
+from ncd_patient_tracking_staging n;
+
+DROP TABLE IF EXISTS ncd_patient_tracking;
+EXEC sp_rename 'ncd_patient_tracking_staging', 'ncd_patient_tracking';
